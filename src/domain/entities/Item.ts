42adedfa9ab3ac058e69785640702741461ec804
Abaranasia <rankirlian@gmail.com>
2024-09-08 17:16:19 +0200

    Update to add update method

--- src/domain/entities/Item.ts
+++ src/domain/entities/Item.ts
@@ -1,6 +1,7 @@
-import { Id } from "../value-objects/Id";
-import { Title, TitleProps } from "../value-objects/Title";
-import { Entity, EntityData } from "./Entity";
+import { Entity, EntityData }  from './Entity';
+
+import { Title } from '../value-objects/Title';
+import { Id } from '../value-objects/Id';
 
 export interface ItemData extends EntityData {
   title: Title;
@@ -44,12 +45,43 @@ export class Item extends Entity {
     // Review if it worths to create an itemId value-object based on Id
     const id = props.id ? Id.createFrom(props.id) : Id.create();
     const title = Title.create(props.title);
-    const subtitle  = Title.create(props.subtitle);;
+    const subtitle = Title.create(props.subtitle);
     const { length } = props;
     const { creationDate } = props;
     const { collected } = props;
     const { collectionId } = props;
 
-    return new Item ({id, title, subtitle, length, creationDate, collected, collectionId})
+    return new Item({
+      id,
+      title,
+      subtitle,
+      length,
+      creationDate,
+      collected,
+      collectionId,
+    });
+  }
+
+  public update(props: Partial<ItemProps>) {
+    // Allows the updating process of an existing item
+    const id = props.id ? Id.createFrom(props.id) : this.id;
+    const title = props.title ? Title.create(props.title) : this.title;
+    const subtitle = props.subtitle
+      ? Title.create(props.subtitle)
+      : this.subtitle;
+    const length = props.length ?? this.length;
+    const creationDate = props.creationDate ?? this.creationDate;
+    const collected = props.collected ?? this.collected;
+    const collectionId = props.collectionId ?? this.collectionId;
+
+    return new Item({
+      id,
+      title,
+      subtitle,
+      length,
+      creationDate,
+      collected,
+      collectionId,
+    });
   }
 }
