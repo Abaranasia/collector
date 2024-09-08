227ce50ce8877044ad8e76455adaa82c82895eb6
Abaranasia <rankirlian@gmail.com>
2024-09-08 19:09:39 +0200

    Style: update spaces and imports

--- src/domain/entities/Collection.ts
+++ src/domain/entities/Collection.ts
@@ -1,11 +1,11 @@
-import { Entity, EntityData } from './Entity';
+import { Entity, EntityData } from "./Entity";
 
-import { Id } from '../value-objects/Id';
-import { Title } from '../value-objects/Title';
-import { Author } from '../value-objects/Author';
+import { Id } from "../value-objects/Id";
+import { Title } from "../value-objects/Title";
+import { Author } from "../value-objects/Author";
 
 export interface CollectionData extends EntityData {
-    name: Title;
+  name: Title;
   author: Author;
   category: string;
 }
@@ -18,15 +18,16 @@ export interface CollectionProps {
 }
 
 export class Collection extends Entity {
-    public readonly name: Title;
-    public readonly author: Author;
-    public readonly category: string;
+  public readonly name: Title;
+  public readonly author: Author;
+  public readonly category: string;
 
   constructor(data: CollectionData) {
     super(data.id);
+
     this.name = data.name;
     this.author = data.author;
-    this.category= data.category; // TODO: category value-object for book/album values
+    this.category = data.category; // TODO: category value-object for book/album values
   }
 
   static create(props: CollectionProps): Collection {
@@ -45,11 +46,10 @@ export class Collection extends Entity {
 
   public update(props: Partial<CollectionProps>) {
     const id = props.id ? Id.createFrom(props.id) : this.id;
-    const name= props.name ?  Title.create(props.name) : this.name;
-    const author= props.author ? Author.create(props.author) : this.author;
-    const category= props.category ?? this.category;
+    const name = props.name ? Title.create(props.name) : this.name;
+    const author = props.author ? Author.create(props.author) : this.author;
+    const category = props.category ?? this.category;
 
-    return new Collection( {id, name, author, category})
-
-}
+    return new Collection({ id, name, author, category });
+  }
 }
