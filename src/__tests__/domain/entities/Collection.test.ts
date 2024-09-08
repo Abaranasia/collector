09be325d1e7e3d9fe5e24d6aa7a32ba92050caa7
Abaranasia <rankirlian@gmail.com>
2024-09-08 23:19:12 +0200

    Update Collection tests to include Category class

--- src/__tests__/domain/entities/Collection.test.ts
+++ src/__tests__/domain/entities/Collection.test.ts
@@ -1,33 +1,35 @@
 import { Collection } from "../../../domain/entities/Collection";
-import { Author } from "../../../domain/value-objects/Author";
 import { Id } from "../../../domain/value-objects/Id";
 import { Title } from "../../../domain/value-objects/Title";
+import { Author } from "../../../domain/value-objects/Author";
+import { Category } from "../../../domain/value-objects/Category";
 
 describe("Test on Collection entity", () => {
   const newCollection = {
     name: "Mistborn series",
     author: "Brandon Sanderson",
-    category: "books",
+    category: "book",
   };
 
   test("should create a Collection with not provided id", () => {
     const mistbornBooks = Collection.create(newCollection);
-    
+
     expect(mistbornBooks).toBeInstanceOf(Collection);
     expect(mistbornBooks.id).toBeInstanceOf(Id);
     expect(mistbornBooks.name).toBeInstanceOf(Title);
     expect(mistbornBooks.author).toBeInstanceOf(Author);
-    
+    expect(mistbornBooks.category).toBeInstanceOf(Category);
+
     expect(mistbornBooks.name.getValue()).toBe(newCollection.name);
     expect(mistbornBooks.author.getValue()).toBe(newCollection.author);
-    expect(mistbornBooks.category).toBe(newCollection.category);
+    expect(mistbornBooks.category.getValue()).toBe(newCollection.category);
   });
 
   test("should update an existing Collection", () => {
     const mistbornBooks = Collection.create(newCollection);
-    const newName ='The Mistborn Series';
+    const newName = "The Mistborn Series";
 
-    const updatedCollection = mistbornBooks.update({name: newName});
+    const updatedCollection = mistbornBooks.update({ name: newName });
 
     expect(updatedCollection.id).toEqual(mistbornBooks.id);
     expect(updatedCollection.author).toEqual(mistbornBooks.author);
@@ -35,25 +37,47 @@ describe("Test on Collection entity", () => {
     expect(updatedCollection.name.getValue()).toBe(newName);
   });
 
-  test('should throw an error when trying to create a collection withour name', () => {
+  test("should throw an error when trying to create a collection without name", () => {
     const errorMsg = "Title is required";
 
-    const invalidCollection= {
-        ...newCollection,
-        name: '',
-      };
-      
+    const invalidCollection = {
+      ...newCollection,
+      name: "",
+    };
+
     expect(() => Collection.create(invalidCollection)).toThrow(errorMsg);
   });
 
-  test('should throw an error when trying to create a collection withour author', () => {
+  test("should throw an error when trying to create a collection without author", () => {
     const errorMsg = "Author is required";
 
-    const invalidCollection= {
-        ...newCollection,
-        author: '',
-      };
-      
+    const invalidCollection = {
+      ...newCollection,
+      author: "",
+    };
+
+    expect(() => Collection.create(invalidCollection)).toThrow(errorMsg);
+  });
+
+  test("should throw an error when trying to create a collection without category", () => {
+    const errorMsg = "Category is required";
+
+    const invalidCollection = {
+      ...newCollection,
+      category: "",
+    };
+
+    expect(() => Collection.create(invalidCollection)).toThrow(errorMsg);
+  });
+
+  test("should throw an error when trying to create a collection with an invalid category", () => {
+    const errorMsg = "Incorrect category";
+
+    const invalidCollection = {
+      ...newCollection,
+      category: "Sport",
+    };
+
     expect(() => Collection.create(invalidCollection)).toThrow(errorMsg);
   });
 });
