18cb1f446c42b508fd61d567cd2581f66666b190
Abaranasia <rankirlian@gmail.com>
2024-09-08 18:55:28 +0200

    Update to remove uneeded private property

--- src/domain/value-objects/Author.ts
+++ src/domain/value-objects/Author.ts
@@ -1,25 +1,22 @@
 import { ValueObject } from "./ValueObject";
 
 export interface AuthorProps {
-  value: string
+  value: string;
 }
 
 export class Author extends ValueObject<AuthorProps> {
-  private value: string;
-
   private constructor(props: AuthorProps) {
     super(props);
-    this.value = props.value;
   }
 
   public static create(author: string): Author {
     if (!author || author.trim().length === 0) {
-      throw new Error('Author is required');
+      throw new Error("Author is required");
     }
-    return new Author({ value: author.trim() })
+    return new Author({ value: author.trim() });
   }
-  
+
   public getValue(): string {
-    return this.value;
+    return this.props.value;
   }
 }
