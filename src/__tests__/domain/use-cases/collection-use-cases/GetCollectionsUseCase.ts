b6c4feed68f298df51e8aa7bbf33541733249d7e
Abaranasia <rankirlian@gmail.com>
2024-09-09 22:59:41 +0200

    add get collection use-case

---
+++ src/__tests__/domain/use-cases/collection-use-cases/GetCollectionsUseCase.ts
@@ -0,0 +1,19 @@
+import { Collection } from "../../../../domain/entities/Collection";
+import { CollectionRepository, GetCollectionsParams } from "../../repositories/CollectionRepository";
+import { PaginatedObjects } from "../../entities/Paginated";
+
+export class GetCollectionsUseCase {
+    constructor(private collectionRepository: CollectionRepository){}
+
+    async execute({ params, headers }: Partial<GetCollectionsParams>): Promise<PaginatedObjects<Collection[]>> {
+        const page = params?.page ?? 1;
+        const count = params?.count ?? 100;
+    
+        return this.collectionRepository.listCollections(
+          {
+            params: { page, count },
+            headers
+          }
+        );
+      }
+}
