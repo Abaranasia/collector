82472613ad7ba6f1318f71bfe3c96911a9ff465d
Abaranasia <rankirlian@gmail.com>
2024-09-09 22:57:52 +0200

    Add apiInstance entity

---
+++ src/domain/entities/ApiInstance.ts
@@ -0,0 +1,4 @@
+import { AxiosInstance, AxiosRequestConfig } from 'axios';
+
+export interface ApiInstance extends AxiosInstance {}
+export interface ApiInstanceHeaders extends AxiosRequestConfig<any> {}
